ad57c8c73e90d6197982634f6f3345cd3606d413
Ania Gonzalez <aniagr@gmail.com>
2016-06-03 09:28:37 -0700

    Added uid, provider, etc. to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603045605) do
+ActiveRecord::Schema.define(version: 20160603162455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,8 +56,14 @@ ActiveRecord::Schema.define(version: 20160603045605) do
     t.string   "email"
     t.string   "password"
     t.string   "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "uid"
+    t.string   "provider"
+    t.string   "url"
+    t.string   "image"
+    t.string   "first_name"
+    t.string   "baby_last_name"
   end
 
 end
